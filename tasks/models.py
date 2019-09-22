6b9d725ad4570a97a9a70087226e7b1b291b64c2
Devansh Ahuja <ahuja.devansh2@gmail.com>
2019-09-22 13:14:54 +0530

    created tasks app migrated task and week_report tables

---
+++ tasks/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from users.models import CustomUser
+
+class Task(models.Model):
+    
+    t_name = models.CharField(max_length=500)
+    duration_from = models.DateTimeField()
+    duration_to = models.DateTimeField()
+    description = models.TextField()
+    members = models.ManyToManyField(CustomUser)
+
+
+class WeeklyReport(models.Model):
+
+    def report_file_path(self, filename):
+        basefilename, file_extension = os.path.splitext(filename)
+        randomstr = datetime.datetime.now().strftime('%d-%m-%Y_%I:%M:%S,%f')
+        return '{userid}/reports/week-{week_number}_{basename}_{randomstring}{ext}'.format(
+            userid=self.user.pk, week_number=self.week_number, basename=basefilename,
+            randomstring=randomstr, ext=file_extension)
+    user = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
+    week_number = models.IntegerField()
+    tasks = models.ManyToManyField(Task)
+    report = models.FileField(upload_to=report_file_path)
